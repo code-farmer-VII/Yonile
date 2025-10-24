87c9a596dd453c205803bc0b489e5d670ea96431
code-farmer-VII <temesgengonfa72127@gmail.com>
2025-10-24 03:50:14 -0700

    fix little issue

--- src/components/Home/InverseClinets.tsx
+++ src/components/Home/InverseClinets.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import { useState } from "react";
 import "../Home/swipClients.css"
 import img1 from "../../assets/Image/Clients/image 66.png"
 import img2 from "../../assets/Image/Clients/image 67.png"
